68f8a50e5c2443d2100b3c8cfda98499dbd37f86
sidsimhadri <sidsimhadri@gmail.com>
2023-10-28 14:31:30 -0500

    dummy itenerary page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,35 +1,3 @@
-// import React from 'react';
-// import { View, StyleSheet } from 'react-native';
-// import { Button, Text } from 'react-native-paper';
-
-// export default function LoginScreen({ navigation }) {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Welcome to Travel Bestie!</Text>
-//       <Button mode="contained" onPress={navigation.navigate('Quiz')}>
-//         Login
-//       </Button>
-//       <Button mode="outlined" onPress={navigation.navigate('Quiz')}>
-//         Sign Up
-//       </Button>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     paddingHorizontal: 20,
-//   },
-//   button: {
-//     padding: 10,
-//   }
-// });
-
-
-
-
 import React, {useState} from 'react';
 import { View, StyleSheet, ScrollView, Dimensions, Image } from 'react-native';
 import {Button, Text} from 'react-native-paper';
